bf39e3a31c61adbad370af7985ee641975d89fe2
Jayden Richardson <jsrichh5@gmail.com>
2022-09-23 20:01:33 +1200

    formatting tidy up

--- main.js
+++ main.js
@@ -105,40 +105,39 @@ var seven = totalIntegers([[[5], 3], 0, 2, ["foo"], [], [4, [5, 6]]]); // 7
 console.log(seven);
 
 //* Write a function that sums squares of numbers in list that may contain more lists
-function SumSquares(array){
-	if(array.length === 0) return 0;
-	let total = 0;
+function SumSquares(array) {
+  if (array.length === 0) return 0;
+  let total = 0;
 
-	for(let i = 0; i < array.length; i++){
-		if(Array.isArray(array[i])){
-			total += SumSquares(array[i]);
-		} else {
-			total += array[i] * array[i];
-		}
-		
-	}
-	return total;
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      total += SumSquares(array[i]);
+    } else {
+      total += array[i] * array[i];
+    }
+  }
+  return total;
 }
 
-var l = [1,2,3]; 
+var l = [1, 2, 3];
 console.log(SumSquares(l)); // 1 + 4 + 9 = 14
 
-l = [[1,2],3]; 
+l = [[1, 2], 3];
 console.log(SumSquares(l)); // 1 + 4 + 9 = 14
 
-l = [[[[[[[[[1]]]]]]]]] 
+l = [[[[[[[[[1]]]]]]]]];
 console.log(SumSquares(l)); // 1 = 1
 
-l = [10,[[10],10],[10]] 
+l = [10, [[10], 10], [10]];
 console.log(SumSquares(l)); // 100 + 100 + 100 + 100 = 400
 
 //* The function should return an array containing repetitions of the number argument. For instance, replicate(3, 5) should return [5,5,5]. If the times argument is negative, return an empty array.
-function replicate(times, number){
-	if(times <= 0) return [];
+function replicate(times, number) {
+  if (times <= 0) return [];
 
-	return [number].concat(replicate(times - 1, number));
+  return [number].concat(replicate(times - 1, number));
 }
 
-console.log(replicate(3, 5)) // [5, 5, 5]
-console.log(replicate(1, 69)) // [69]
-console.log(replicate(-2, 6)) // []
+console.log(replicate(3, 5)); // [5, 5, 5]
+console.log(replicate(1, 69)); // [69]
+console.log(replicate(-2, 6)); // []
